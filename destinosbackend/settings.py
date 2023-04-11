f99a20be67555141f236916badc45a8f4324c525
federicos1 <fedes7777@gmail.com>
2023-04-11 12:55:46 -0300

    init

--- destinosbackend/settings.py
+++ destinosbackend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@k_r2vhs6(xv=h==^=w0ukufj60tfwj^-m8(jct-h(c)d44b4t'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
